559fa2614273f6b8998e17d4b8de44993c7f96f2
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-10-20 11:12:26 +0600

    qrcode changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
     }
   };
 
-  const mblDataPass = "www.neurologylibrary.org";
+  const mblDataPass = "http://www.neurologylibrary.org";
   const pcDataPass = "Dementa";
 
   //for login system
